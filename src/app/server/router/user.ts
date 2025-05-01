3cee3237bc9fedad613aa9a32bd0a5c39876a0e0
Septian Padli <m.septianpadli@gmail.com>
2025-05-01 16:41:52 +0700

    add: Sinkronisasi Clerk ke DB

--- src/app/server/router/user.ts
+++ src/app/server/router/user.ts
@@ -1,10 +1,40 @@
-import { publicProcedure, router } from '../trpc'
-import prisma from '@/lib/prisma'
+import { protectedProcedure, publicProcedure, router } from "../trpc";
+import prisma from "@/lib/prisma";
+import { retryConnect } from "@/lib/utils";
+import { z } from "zod";
 
 export const userRouter = router({
-  getUsers: publicProcedure.query(async () => {
-    return await prisma.user.findMany()
-  }),
-})
+	syncWithSupabase: protectedProcedure
+		.input(
+			z.object({
+				id: z.string(),
+				name: z.string(),
+				email: z.string().email(),
+			})
+		)
+		.mutation(async ({ input }) => {
+			const existingUser = await retryConnect(() =>
+				prisma.user.findFirst({
+					where: {
+						OR: [{ id: input.id }, { email: input.email }],
+					},
+				})
+			);
 
-export type UserRouter = typeof userRouter
+			if (!existingUser) {
+				return await prisma.user.create({
+					data: {
+						id: input.id,
+						name: input.name,
+						email: input.email,
+					},
+				});
+			}
+		}),
+
+	getUsers: publicProcedure.query(async () => {
+		return await prisma.user.findMany();
+	}),
+});
+
+export type UserRouter = typeof userRouter;
