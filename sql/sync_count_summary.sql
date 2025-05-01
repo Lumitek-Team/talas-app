f81e668e7b5049ad67ecdd05dd18715547433c7e
Septian Padli <m.septianpadli@gmail.com>
2025-05-01 22:15:57 +0700

    add: env example

--- sql/sync_count_summary.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION handle_new_user()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Insert into count_summary with a generated ULID
-    INSERT INTO count_summary (id, id_user)
-    VALUES (ulid_generate(), NEW.id); -- Generate a ULID for the `id` field
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
