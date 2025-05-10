3c8fe461f0fb43d474e3adcb6810bcef5706e2c8
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-05-10 20:51:59 +0700

    fix : tech icon scrolling animation

--- src/components/landing/techIcons.tsx
+++ src/components/landing/techIcons.tsx
@@ -13,11 +13,20 @@ const duplicatedTech = [...tech, ...tech, ...tech];
 
 export function TechIcons() {
   return (
-    <div className="my-40 overflow-hidden w-full h-[120px]">
+    <div className="my-40 overflow-hidden w-full h-[160px]">
       <div className="flex animate-scroll whitespace-nowrap gap-40 min-w-[300%]">
         {duplicatedTech.map((t, i) => (
-          <div key={i} className="inline-flex items-center px-5">
-            <Image src={t.src} alt={t.name} width={100} height={100} />
+          <div
+            key={i}
+            className="inline-flex items-center justify-center flex-shrink-0 w-[140px] h-[140px] px-5"
+          >
+            <Image
+              src={t.src}
+              alt={t.name}
+              width={140}
+              height={140}
+              className="w-full h-full object-contain"
+            />
           </div>
         ))}
       </div>
