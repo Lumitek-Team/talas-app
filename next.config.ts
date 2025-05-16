98a96c9c4b489595feb7e5190d850b2d52df6fe8
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-05-16 18:21:16 +0700

    The images.domains configuration is deprecated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["ivpwvybbpejezeeqxsnn.supabase.co"], // Add your Supabase storage domain here
+    // domains: ["ivpwvybbpejezeeqxsnn.supabase.co"], // Deprecated approach
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ivpwvybbpejezeeqxsnn.supabase.co', // Add your Supabase storage domain here
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
